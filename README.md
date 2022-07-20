ba0bf15821842e703ca46d7903e4324f661d6415
ModPunchtree <modpunchtree@gmail.com>
2022-07-20 02:53:26 +0100

    V1 of the emulator is complete. Currently the CPU comes with a tiny program which allows the user to write whatever they want to the RAM.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# MarshmellowEmulator
+# Marshmellow Emulator
 The simplest possible 16 bit Von Neumann CPU that is designed specifically to run Core URCL.
+
+# Specs
+* 8x4 character terminal
+* 4k 16 bit words of RAM
+* 12 bit program counter
+
+## 8 Main Registers:
+*   R0          - Constant 0
+*   R1 to R5    - General purpose registers (16 bit)
+*   SP          - Stack pointer (physically it is just a normal register like R1 to R5) (16 bit)
+*   PC          - Program counter (12 bit)
+
+```
+Instructions  Unused      Op Code     Op1     Op2     Op3
+         ADD    XXXX          000     CCC     AAA     BBB
+         BGE    XXXX          001     CCC     AAA     BBB
+         NOR    XXXX          010     CCC     AAA     BBB
+         RSH    XXXX          011     CCC     AAA     000
+         LOD    XXXX          100     CCC     AAA     000
+         STR    XXXX          101     000     AAA     BBB
+          IN    XXXX          110     CCC     000     000
+         OUT    XXXX          111     000     AAA     000
+
+Registers     Index
+       R0       000
+       R1       001
+       R2       010
+       R3       011
+       R4       100
+       R5       101
+       SP       110
+       PC       111
+```
+## Note
+IMM instructions are not directly supported by the CPU - instead you must load a RAM location which contains the desired immediate value.
---
+++ main.py
@@ -0,0 +1,205 @@
+
+# Marshmellow Emulator
+# A simple 16 bit CPU designed to run Core URCL
+
+# 8x4 character terminal
+# 4k of RAM (12 bit program counter)
+
+# 8 registers
+#   R0          - Constant 0
+#   R1 to R5    - General purpose registers (16 bit)
+#   SP          - Stack pointer (physically it is just a normal register like R1 to R5) (16 bit)
+#   PC          - Program counter (12 bit)
+
+# Instructions  Unused      Op Code     Op1     Op2     Op3
+#          ADD    XXXX          000     CCC     AAA     BBB
+#          BGE    XXXX          001     CCC     AAA     BBB
+#          NOR    XXXX          010     CCC     AAA     BBB
+#          RSH    XXXX          011     CCC     AAA     000
+#          LOD    XXXX          100     CCC     AAA     000
+#          STR    XXXX          101     000     AAA     BBB
+#           IN    XXXX          110     CCC     000     000
+#          OUT    XXXX          111     000     AAA     000
+
+# Registers     Index
+#        R0       000
+#        R1       001
+#        R2       010
+#        R3       011
+#        R4       100
+#        R5       101
+#        SP       110
+#        PC       111
+
+# Note - IMM instructions are not directly supported by the CPU, instead you must load a RAM location which contains the desired immediate value
+
+def fix16bit(x: int) -> int:
+    while x >= (2**16):
+        x -= (2**16)
+    while x < 0:
+        x += (2**16)
+    return x
+
+def logicalNOR(x: int, y: int) -> int:
+    x = bin(x)[2: ]
+    while len(x) < 16:
+        x = "0" + x
+    y = bin(y)[2: ]
+    while len(y) < 16:
+        y = "0" + y
+    answer = ""
+    for a, b in x, y:
+        if (a == 0) and (b == 0):
+            answer += "1"
+        else:
+            answer += "0"
+    answer = int(answer, 2)
+    return answer
+
+def charSet() -> tuple:
+    return ('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','_','?','>','<','=','-','+','¦¦','Σ','(',')','/','\\','^','.','‾',',',"'",'¦','≡','!','"','°','\n',' ')
+
+def printChar(a: int, display: list) -> list:
+    char = charSet()[a & 63]
+    if char == '\n':
+        for x in range(8):
+            for y in range(4):
+                if y == 0:
+                    display[x][y] = ' '
+                else:
+                    display[x][y] = display[x][y - 1]
+    else:
+        for x in range(7):
+            display[x][0] = display[x + 1][0]
+        display[7][0] = char
+    
+    # redraw screen
+    drawScreen(display)
+    
+    return display
+
+def drawScreen(display: list) -> None:
+    screen = ""
+    for y in range(4):
+        for x in range(8):
+            screen += display[x][y]
+        screen += '\n'
+    screen = screen[: -1]
+    print("\n\n\n")
+    print(screen)
+
+ram = [0] * (2**12)
+registers = [0] * (2**3)
+display = [[' '] * 4] * 8
+
+R0 = 0
+R1 = 1
+R2 = 2
+R3 = 3
+R4 = 4
+R5 = 5
+SP = 6
+PC = 7
+
+ADD = 0
+BGE = 1
+NOR = 2
+RSH = 3
+LOD = 4
+STR = 5
+IN  = 6
+OUT = 7
+
+# initialise RAM with a program
+memoryEditor = [
+    0x0C40, # IN R1 %NUMB
+    0x0C80, # IN R2 %NUMB
+    0x0A0A, # STR R1 R2
+    0x0200  # BGE R0 R0 R0
+]
+terminal = [
+    0x0000
+]
+
+startingProgram = memoryEditor
+ram = startingProgram + ram[len(startingProgram): ]
+
+
+registers[SP] = (2**12) - 1
+errorCount = 0
+drawScreen(display)
+
+while True:
+    # fetch instruction
+    instruction = ram[registers[PC]]
+
+    opCode = (instruction & 0x0E00) >> 9
+    op1    = (instruction & 0x01C0) >> 6
+    op2    = (instruction & 0x0038) >> 3
+    op3    = (instruction & 0x0007)
+    
+    dontIncrement = False
+    
+    if opCode == ADD:
+        source1        = registers[op2]
+        source2        = registers[op3]
+        answer         = fix16bit(source1 + source2)
+        registers[op1] = answer
+        
+    elif opCode == BGE:
+        source1        = registers[op2]
+        source2        = registers[op3]
+        destination    = registers[op1]
+        answer         = (source1 + (((2**16) - 1) - source2) + 1) >= (2**16)
+        if answer:
+            registers[PC] = destination
+            dontIncrement = True
+    
+    elif opCode == NOR:
+        source1        = registers[op2]
+        source2        = registers[op3]
+        answer         = logicalNOR(source1, source2)
+        registers[op1] = answer
+    
+    elif opCode == RSH:
+        source1        = registers[op2]
+        answer         = source1 >> 1
+        registers[op1] = answer
+    
+    elif opCode == LOD:
+        address        = registers[op2] & 0x03FF
+        registers[op1] = ram[address]
+    
+    elif opCode == STR:
+        address        = registers[op2] & 0x03FF
+        ram[address]   = registers[op3]
+    
+    elif opCode == IN:
+        while True:
+            userInput = input("INPUT: ")
+            if userInput[: 1].isnumeric():
+                userInput = int(userInput, 0)
+                break
+            elif userInput in charSet():
+                userInput = charSet().index(userInput)
+                break
+            else:
+                print(f"Invalid input: {userInput}\nOnly 16 bit unsigned numbers or chars in the char set are valid")
+            
+        registers[op1] = fix16bit(userInput)
+    
+    elif opCode == OUT:
+        source1        = registers[op2]
+        printChar(source1)
+    
+    else:
+        raise Exception("FATAL - Attempted to exectute an unrecognised instruction")
+
+    registers[R0] = 0
+    if not dontIncrement:
+        registers[PC] += 1
+        
+    registers[PC] &= 0x03FF
+
+
+
